faaaf5906aad3296e14c30957a5d834a527796e5
christysawotong <sawotongchristy@gmail.com>
2025-11-26 13:18:05 +0800

    Add files via upload

---
+++ data_menu_makanan.py
@@ -0,0 +1,19 @@
+import random
+import os
+import time
+
+# ======================
+#       DATA MENU
+# ======================
+
+menu_makanan = {
+    "Nasi Goreng": 18000,
+    "Ayam Geprek": 17000,
+    "Mie Ayam": 15000,
+    "Soto Ayam": 16000,
+    "Bakso": 14000,
+    "Nasi Padang": 22000,
+    "Gado-Gado": 13000,
+    "Ayam Panggang": 25000,
+    "Mie Goreng": 12000
+}
